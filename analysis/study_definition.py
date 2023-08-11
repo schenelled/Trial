8ce559eba3966f816d3b6511f91d4581e0577eb0
schenelled <schenelle.dlima@postgrad.manchester.ac.uk>
2023-08-11 15:17:23 +0100

    Generate age histograms

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,6 +1,7 @@
 from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
 
 
+
 study = StudyDefinition(
     default_expectations={
         "date": {"earliest": "1900-01-01", "latest": "today"},
@@ -9,5 +10,13 @@ study = StudyDefinition(
     },
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2020-02-01"
+    ),
+
+age=patients.age_as_of(
+        "2019-09-01",
+        return_expectations={
+            "rate": "universal",
+            "int": {"distribution": "population_ages"},
+        },
     ),
 )
